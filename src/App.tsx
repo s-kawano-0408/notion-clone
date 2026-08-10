ca851342dc16925905d5c0838972fdb245e22983
kawano <seiya0408s@gmail.com>
2026-08-10 09:54:16 +0900

    feat: add application routes
    アプリケーションのルーティングを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import Layout from "./Layout";
+import NoteDetail from "./pages/NoteDetail";
+
 function App() {
   return (
-    <>
-      <div>Hello World</div>
-    </>
+    <BrowserRouter>
+      <div className="app-container">
+        <Routes>
+          <Route path="/signin" element={<Signin />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />}></Route>
+            <Route path="/notes/:id" element={<NoteDetail />}></Route>
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
